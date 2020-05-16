f453d75134dd13b327e1d316b37bf565175dfa9c
glabka <glabka333@gmail.com>
2020-05-16 14:51:58 +0200

    Method toString added. Methods dispose and disposeAll were made static.

--- domain/Company.java
+++ domain/Company.java
@@ -3,27 +3,27 @@ package domain;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Company implements BankAccountOwner{
+public class Company implements BankAccountOwner {
 
 	/**
 	 * key is id of company
 	 */
 	private static Map<Integer, Company> companies = new HashMap<Integer, Company>();
-	
+
 	private int id;
 	private String name;
 	private String headquarters;
 	private Person executiveDirector;
-	
+
 	private Company(int id, String name, String headquarters, Person executiveDirector) {
 		this.name = name;
 		this.headquarters = headquarters;
-		this.companies.put(id, this);
+		companies.put(id, this);
 	}
-	
+
 	/**
-	 * Creates new instance of company, if not already instantiated - in that case return already instantiated 
-	 * object with unchanged field values.
+	 * Creates new instance of company, if not already instantiated - in that case
+	 * return already instantiated object with unchanged field values.
 	 * 
 	 * @param name
 	 * @param headquarters
@@ -31,21 +31,22 @@ public class Company implements BankAccountOwner{
 	 */
 	public static Company getInstance(int id, String name, String headquarters, Person executiveDirector) {
 		Company company = companies.get(id);
-		if(company != null) {
+		if (company != null) {
 			return company;
 		} else {
 			return new Company(id, name, headquarters, executiveDirector);
 		}
 	}
-	
+
 	/**
 	 * Might return null in case company is not instantiated
+	 * 
 	 * @param name
 	 */
 	public static Company getInstance(int id) {
 		return companies.get(id);
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -65,7 +66,7 @@ public class Company implements BankAccountOwner{
 	public void setExecutiveDirector(Person executiveDirector) {
 		this.executiveDirector = executiveDirector;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
@@ -74,27 +75,37 @@ public class Company implements BankAccountOwner{
 		this.name = name;
 	}
 
+	@Override
+	public String toString() {
+		return "[" + this.getId() + ", " + this.getName() + ", " + this.getHeadquarters() + ", "
+				+ this.getExecutiveDirector() + "]";
+	}
+
 	/**
-	 * Should be used after the end of usage of certain instance of Company to free allocated
-	 * memory associated with this object. 
+	 * Should be used after the end of usage of certain instance of Company to free
+	 * allocated memory associated with this object.
 	 * 
-	 * BE AWARE that when used in any other way then right before loosing all references to instance of Company
-	 * or its end of use can lead to existence of two Company instances of one actual person.
+	 * BE AWARE that when used in any other way then right before loosing all
+	 * references to instance of Company or its end of use can lead to existence of
+	 * two Company instances of one actual person.
+	 * 
 	 * @param company
 	 */
-	public void dispose(Company company) {
+	public static void dispose(Company company) {
 		companies.remove(company.getId());
 	}
-	
+
 	/**
-	 * Should be used after the end of usage of all instances of Company class to free allocated
-	 * memory associated with these objects. 
+	 * Should be used after the end of usage of all instances of Company class to
+	 * free allocated memory associated with these objects.
 	 * 
-	 * BE AWARE that when used in any other way then right before loosing all references to all instances of Company
-	 * or end of use of all instance can lead to existence of two Company instances of one actual person.
+	 * BE AWARE that when used in any other way then right before loosing all
+	 * references to all instances of Company or end of use of all instance can lead
+	 * to existence of two Company instances of one actual person.
+	 * 
 	 * @param person
 	 */
-	public void disposeAll() {
+	public static void disposeAll() {
 		companies.clear();
 	}
 }
